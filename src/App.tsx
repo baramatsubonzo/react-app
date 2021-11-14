eb9ef8a0e6f6eb3413b085dc8ba5d420642a542f
baramatsubonzo <baramatsubonzo@gmail.com>
2021-11-14 18:57:24 +0900

    globalなスタイルの設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Button, ChakraProvider } from "@chakra-ui/react";
-import "./styles.css";
+import theme from "./theme/theme";
 
 export default function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Button>ボタン</Button>
+      <p>aaaaa</p>
     </ChakraProvider>
   );
 }
