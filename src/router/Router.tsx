31c67850424edbedf68eb92fe6ff0019e59db21e
baramatsubonzo <baramatsubonzo@gmail.com>
2021-11-14 19:28:46 +0900

    404ページの導入

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,8 +1,9 @@
 import { memo, VFC } from "react";
 import { Route, Switch } from "react-router-dom";
 
-import { Login } from "../components/pages/Login"
+import { Login } from "../components/pages/Login";
 import { homeRoutes } from "./HomeRoutes";
+import { Page404 } from "../components/pages/Page404";
 
 export const Router: VFC = memo(() => {
   return (
@@ -23,6 +24,9 @@ export const Router: VFC = memo(() => {
           ))}
         </Switch>
       )} />
+      <Route path="*">
+        <Page404 />
+      </Route>
     </Switch>
   )
 });
