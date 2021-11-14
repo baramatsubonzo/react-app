24c32f49434bde195b78e18074fb974eeda09d1d
baramatsubonzo <baramatsubonzo@gmail.com>
2021-11-14 21:47:11 +0900

    ヘッダーのレイアウトの修正2

--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,4 +1,4 @@
-import { Flex, Heading } from "@chakra-ui/layout";
+import { Box, Flex, Heading, Link } from "@chakra-ui/layout";
 import { memo, VFC } from "react";
 
 export const Header: VFC = memo(() => {
@@ -8,12 +8,20 @@ export const Header: VFC = memo(() => {
       bg="teal.500"
       color="gray.50"
       align="center"
-      justify="spave-between"
+      justify="space-between"
       padding={{ base: 3, md: 5 }}
     >
-      <Heading as="h1" fontSize={{ base: "md", md: "lg"}}>
-        ユーザー管理アプリ
-      </Heading>
+      <Flex align="center" as="a" mr={8} _hover={{ cursor: "pointer" }}>
+        <Heading as="h1" fontSize={{ base: "md", md: "lg"}}>
+          ユーザー管理アプリ
+        </Heading>
+      </Flex>
+      <Flex align="center" fontSize="sm" flexGrow={2} display={{ base: "none",  md: "flex"}}>
+        <Box pr={4}>
+          <Link>ユーザー一覧</Link>
+        </Box>
+        <Link>設定</Link>
+      </Flex>
     </Flex>
   )
 });
