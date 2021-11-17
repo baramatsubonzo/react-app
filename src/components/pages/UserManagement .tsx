a04730c5803ac39dd1521f09e12c11b9bfe0cf30
baramatsubonzo <baramatsubonzo@gmail.com>
2021-11-18 08:29:15 +0900

    ユーザーカードのcomponentの作成

--- src/components/pages/UserManagement .tsx
+++ src/components/pages/UserManagement .tsx
@@ -1,16 +1,36 @@
 import { memo, VFC } from "react";
-import { Wrap, WrapItem } from "@chakra-ui/react";
+import { Box, Image, Stack, Text, Wrap, WrapItem } from "@chakra-ui/react";
 
 export const UserManagement: VFC = memo(() => {
   return (
-    <Wrap spacing="30px">
-      {[...Array(10)].map(() => (
-        <WrapItem>
-          <div
-            style={{ width: "100px", height: "100px", backgroundColor: "teal" }}
-          />
-        </WrapItem>
-      ))}
+    <Wrap p={{ base: 4, md: 10 }}>
+      <WrapItem>
+        <Box
+          w="200px"
+          h="260px"
+          bg="white"
+          borderRadius="10px"
+          shadow="md"
+          p={4}
+          _hover={{ cursor: "pointer", opacity: 0.8 }}
+        >
+          <Stack textAlign="center">
+            <Image
+              borderRadius="full"
+              boxSize="160px"
+              src="https://source.unsplash.com/random"
+              alt="プロフィール画像"
+              m="auto"
+            />
+            <Text fontSize="lg" fontWeight="bold">
+              bonzo
+            </Text>
+            <Text fontSize="sm" color="gray">
+              Shota Matsubara
+            </Text>
+          </Stack>
+        </Box>
+      </WrapItem>
     </Wrap>
   )
 });
