c6dccd36ee971c30b0dc967c2542cabcd0ac0067
baramatsubonzo <baramatsubonzo@gmail.com>
2021-11-20 13:38:18 +0900

    ログインユーザー情報をContextに保持する

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -4,23 +4,31 @@ import { useHistory } from "react-router-dom";
 
 import { User } from "../types/api/user";
 import { useMessage } from "./useMessage";
+import { useLoginUser } from "./useLoginUser";
 
 export const useAuth = () => {
   const history = useHistory();
   const { showMessage } = useMessage();
   const [loading, setLoding] = useState(false);
+  const { setLoginUser } = useLoginUser();
 
   const login = useCallback((id: string) => {
     setLoding(true);
 
     axios.get<User>(`https://jsonplaceholder.typicode.com/users/${id}`).then((res)=>{
       if (res.data) {
+        setLoginUser(res.data);
         showMessage({ title: "ログインしました", status: "success"});
         history.push("/home");
       } else {
         showMessage({ title: "ユーザーが見つかりません", status: "error"});
+        setLoding(false);
       }
-    }).catch(() => showMessage({ title: "ログインできません", status: "error"})).finally(() => setLoding(false));
-  }, [history, showMessage]);
+    })
+    .catch(() => {
+      showMessage({ title: "ログインできません", status: "error"})
+      setLoding(false);
+    });
+  }, [history, showMessage, setLoginUser]);
   return { login, loading };
 }
