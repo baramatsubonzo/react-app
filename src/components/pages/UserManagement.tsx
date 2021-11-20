f9ba10aa7ac875fe2b5c63a636d7e4ba53b3b30d
baramatsubonzo <baramatsubonzo@gmail.com>
2021-11-20 11:37:31 +0900

    ユーザー詳細モーダル機能の実装

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -11,17 +11,18 @@ import {
 import { UserCard } from "../organisms/user/UserCard";
 import { useAllUsers } from "../../hooks/useAllUsers";
 import { UserDetailModal } from "../organisms/user/UserDetailModal";
+import { useSelectUser } from "../../hooks/useSelectUser";
 
 export const UserManagement: VFC = memo(() => {
   const { isOpen, onOpen, onClose } = useDisclosure()
   const { getUsers, users, loading } = useAllUsers()
+  const { onSelectUser, selectedUser } = useSelectUser();
 
   useEffect(() => getUsers(), [])
 
   const onClickUser = useCallback((id: number) => {
-    console.log(id);
-    onOpen()
-  }, [])
+    onSelectUser({ id, users, onOpen });
+  }, [users, onSelectUser, onOpen])
 
   return (
     <>
@@ -44,7 +45,7 @@ export const UserManagement: VFC = memo(() => {
             ))}
           </Wrap>
         )}
-      <UserDetailModal isOpen={isOpen} onClose={onClose}/>
+      <UserDetailModal user={selectedUser} isOpen={isOpen} onClose={onClose}/>
     </>
   );
 });
