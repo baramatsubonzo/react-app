ed5eee99cd4aba3e4399236efa14c521d17b5826
baramatsubonzo <baramatsubonzo@gmail.com>
2021-11-20 14:10:33 +0900

    管理者フラグの用意

--- src/providers/LoginUserProvider.tsx
+++ src/providers/LoginUserProvider.tsx
@@ -2,10 +2,12 @@ import { createContext, Dispatch, SetStateAction, ReactNode, useState } from "re
 
 import { User } from "../types/api/user"
 
+type LoginUser = User & { isAdmin: boolean };
+
 
 export type LoginUserContextType = {
-  loginUser: User | null
-  setLoginUser: Dispatch<SetStateAction<User | null>>
+  loginUser: LoginUser | null
+  setLoginUser: Dispatch<SetStateAction<LoginUser | null>>
 }
 
 export const LoginUserContext = createContext<LoginUserContextType>(
@@ -14,7 +16,7 @@ export const LoginUserContext = createContext<LoginUserContextType>(
 
 export const LoginUserProvider = (props: {children: ReactNode}) => {
   const { children } = props
-  const [loginUser, setLoginUser] = useState<User | null>(null);
+  const [loginUser, setLoginUser] = useState<LoginUser | null>(null);
   return (
     <LoginUserContext.Provider value={{ loginUser, setLoginUser }}>
       {children}
