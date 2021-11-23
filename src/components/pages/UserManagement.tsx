bbda55af7ab24f5d84c7fc936013017a2c3ca536
baramatsubonzo <baramatsubonzo@gmail.com>
2021-11-23 21:47:21 +0900

    管理者ユーザーのみユーザー情報を編集できる導線の作成

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -19,7 +19,6 @@ export const UserManagement: VFC = memo(() => {
   const { getUsers, users, loading } = useAllUsers()
   const { onSelectUser, selectedUser } = useSelectUser();
   const { loginUser } = useLoginUser();
-  console.log(loginUser);
 
   useEffect(() => getUsers(), [])
 
@@ -48,7 +47,7 @@ export const UserManagement: VFC = memo(() => {
             ))}
           </Wrap>
         )}
-      <UserDetailModal user={selectedUser} isOpen={isOpen} onClose={onClose}/>
+      <UserDetailModal user={selectedUser} isOpen={isOpen} isAdmin={loginUser?.isAdmin} onClose={onClose}/>
     </>
   );
 });
